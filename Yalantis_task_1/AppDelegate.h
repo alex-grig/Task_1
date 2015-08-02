19d2d69f4eb44d5084422b16354fea1a96dba56f
alex <donlehandro@yandex.ru>
2015-08-02 11:01:05 +0300

    Initial Commit

---
+++ Yalantis_task_1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yalantis_task_1
+//
+//  Created by robert on 02.08.15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Yalantis_task_1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Yalantis_task_1
+//
+//  Created by robert on 02.08.15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
